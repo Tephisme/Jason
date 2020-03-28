32823f58022ec2259cad519a73b646fb7d896939
Tephisme <48565929+Tephisme@users.noreply.github.com>
2020-03-28 21:25:21 +0100

    Added IHaveHealth.cs and Player.cs for Jason to look at.

---
+++ IHaveHealth.cs
@@ -0,0 +1,7 @@
+﻿public interface IHaveHealth
+{
+    int Health { get; }
+
+    void TakeDamage(int damageAmount);
+    void Heal(int healAmount);
+}
---
+++ Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour, IHaveHealth
+{
+    [SerializeField] private int _Health = 100;
+    
+    [Range(1,1000)] public int MaxHealth = 100;
+
+    public int Health
+    {
+        get { return _Health; }
+        private set
+        {
+            _Health = Mathf.Clamp(value, 0, MaxHealth); 
+            if (IsDead)
+                Die();
+        }
+    }
+
+    public bool IsDead { get => Health <= 0;  }
+
+    private void OnValidate()
+    {
+        Health = _Health;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        Health -= damageAmount;
+    }
+
+    public void Heal(int healAmount)
+    {
+        Health += healAmount;
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player is dead.");
+    }
+    
+    [ContextMenu("Take Lethal Damage")]
+    private void TakeLethalDamage()
+    {
+        TakeDamage(Health);
+    }
+}
